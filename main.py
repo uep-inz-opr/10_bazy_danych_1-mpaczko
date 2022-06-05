2790eef4a63a833dd338ecb51294c0b0cb03c8b3
Michal <michalpaczko@gmail.com>
2022-06-06 00:13:38 +0200

    test1

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import pymysql	
+	
+connection = pymysql.connect(host = "ego.kie.ue.poznan.pl", port = 8080, user="inz_opr", passwd="inz_opr", database="inzynieria_oprogramowania")
+cursor = connection.cursor()
+sql = "select * from polaczenia;"
+cursor.execute(sql)
+suma=0
+
+for i in cursor:
+	suma+=i[3]
+	print(suma)
 
 if __name__ == "__main__":
     pass
